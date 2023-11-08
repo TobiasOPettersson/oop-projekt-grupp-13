232bb8971f2032f124d3e8689a5bb64cf8ba283d
TobiasOPettersson <tobias.pettersson99@hotmail.com>
2023-11-08 09:01:30 +0100

    added attributes and methods for AEnemy

--- Model/AEnemy.java
+++ Model/AEnemy.java
@@ -1,4 +1,39 @@
 package Model;
 
-public class AEnemy {
+public abstract class AEnemy {
+    private int health; //The health points of an enemy
+    private double posX, posY;
+    private double speedX, speedY; //The speed in x- and y-direction for an enemy
+
+    public AEnemy(int health, double posX, double posY, double speedX, double speedY) {
+        this.health = health;
+        this.posX = posX;
+        this.posY = posY;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+
+    public void move(double x, double y) {
+        this.posX += x;
+        this.posY += y;
+    }
+    public int getHealth(){
+        return this.health;
+    }
+
+    public double getPosX() {
+        return this.posX;
+    }
+
+    public double getPosY() {
+        return this.posY;
+    }
+
+    public double getSpeedX() {
+        return this.speedX;
+    }
+
+    public double getSpeedY() {
+        return this.speedY;
+    }
 }
