9a4e345eb6fb64356021b84f818f8a97238af835
OxyRust <97917116+OxyRust@users.noreply.github.com>
2023-11-08 11:28:57 +0100

    Basic Window

---
+++ View/TempMain.java
@@ -0,0 +1,11 @@
+package View;
+import View.GameView;
+import Model.MainModel;
+
+public class TempMain {
+    
+    public static void main(String[] args){
+        MainModel model = new MainModel();
+        GameView view = new GameView(model);
+    }
+}
