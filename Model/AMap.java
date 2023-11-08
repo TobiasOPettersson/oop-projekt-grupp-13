ce389d80bb0842e4fd0cd4219fb42b0eeb6d6837
Simbpa <113666561+Simbpa@users.noreply.github.com>
2023-11-08 10:00:57 +0100

    Made enum TileAvailability and first changes to AMap

--- Model/AMap.java
+++ Model/AMap.java
@@ -1,4 +1,22 @@
 package Model;
 
+import java.util.List;
+
+import Model.TileAvailability.GridType;
+
+import java.util.ArrayList;
+
 public class AMap {
+    public List<ATower> towers = new ArrayList<ATower>();
+    public List<AEnemy> enemies = new ArrayList<AEnemy>();
+    public GridType grid [][] = new GridType[10][8];
+
+    public void createGrid(int[] positionX, int[] positionY){
+        for(int i = 0 ; i < grid.length ; i++){
+            for(int j = 0 ; i < grid[0].length ; j++){
+                grid[j][i] = GridType.Free;
+            }
+        }
+        System.out.println(grid);
+    }
 }
---
+++ Model/TileAvailability.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class TileAvailability {
+    enum GridType {
+        Free,
+        Tower,
+        Path
+    }
+}
