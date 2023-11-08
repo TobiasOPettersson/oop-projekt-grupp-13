307d5df7ebd3e6e5d24be76791a80986d16639a1
EliasNe <89510013+EliasNe@users.noreply.github.com>
2023-11-08 10:28:07 +0100

    Skeleton for abstract/upgrade/create -towers
    Gjorde skalet för dessa tre klasser, behöver ändå fixa logiken kring metoderna i upgrade and create tower in klasserna upgrade/create towers

--- Controller/CreateTowerController.java
+++ Controller/CreateTowerController.java
@@ -1,4 +1,28 @@
 package Controller;
 
-public class CreateTowerController {
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import Model.ATower;
+
+public class CreateTowerController extends TowerController {
+
+    public CreateTowerController(int x, int y, List<JButton> buttons, JLabel label, JPanel panel) {
+        super(x, y, buttons, label, panel);
+        // TODO Auto-generated constructor stub
+    }
+
+    // method TODO Metbod createTower
+    // TODO: Implement tower creation logic here based on the provided coordinates
+
+    // public void createTower(int x, int y, ATower tower) {
+    // Example: Creating a tower object and adding it to the game world.
+    // ATower newTower = new ATower(x, y, tower);
+    // Tell mainModel to create the tower..
+    // MainModel.addTower(newTower);
+    // }
+
 }
