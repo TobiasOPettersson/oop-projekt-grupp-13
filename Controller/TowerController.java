e191447925a4689ee83b21f85a759f54512b412f
EliasNe <89510013+EliasNe@users.noreply.github.com>
2023-11-08 10:29:09 +0100

    Revert "Skeleton for abstract/upgrade/create -towers"

--- Controller/TowerController.java
+++ Controller/TowerController.java
@@ -1,40 +1,4 @@
 package Controller;
 
-/**
- * This is an abstract class representing a Tower Controller in a game.
- * It defines the common properties and behaviors for tower controllers.
- * Subclasses are expected to extend this class and provide specific implementations.
- * 
- * Properties:
- * - x: X-coordinate of the tower's position.
- * - y: Y-coordinate of the tower's position.
- * - buttons: A list of JButton objects associated with the tower.
- * - label: A JLabel object associated with the tower.
- * - panel: A JPanel object associated with the tower.
- 
- */
-
-import java.util.List;
-import java.util.ArrayList;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-public abstract class TowerController {
-
-    private int x;
-    private int y;
-    private List<JButton> buttons = new ArrayList<>();
-    private JLabel label;
-    private JPanel panel;
-
-    public TowerController(int x, int y, List<JButton> buttons, JLabel label, JPanel panel) {
-        this.x = x;
-        this.y = y;
-        this.buttons = new ArrayList<>(buttons);
-        this.label = label;
-        this.panel = panel;
-    }
-
+public class TowerController {
 }
