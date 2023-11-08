92b626198d015cc71a3138fc315ffc2c63437bb3
TobiasOPettersson <tobias.pettersson99@hotmail.com>
2023-11-08 09:37:09 +0100

    EnemyOne, subclass of AEnemy

--- Model/EnemyOne.java
+++ Model/EnemyOne.java
@@ -1,4 +1,13 @@
 package Model;
 
-public class EnemyOne {
+public class EnemyOne extends AEnemy{
+    
+    public EnemyOne(int health, double posX, double posY, double speedX, double speedY) {
+        super(health, posX, posY, speedX, speedY);
+    }
+
+    @Override
+    public String toString() {
+        return "EnemyOne with health: " + getHealth() + ", " + "position: (" + getPosX() + "," + getPosY() + ")" + " speed: (" + getSpeedX() + "," + getSpeedY() + ")";
+    }
 }
