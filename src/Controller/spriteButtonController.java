c94c142922b7f298cf76179dddd98ae17034a300
EliasNe <89510013+EliasNe@users.noreply.github.com>
2023-11-16 11:31:17 +0100

    spritButtonClassImage
    added a spritebuttonclassImage for my button to show a image...

---
+++ src/Controller/spriteButtonController.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+class spriteButtonController extends JButton {
+
+    private BufferedImage spriteImage;
+
+    public spriteButtonController(String imagePath) {
+        setOpaque(false); // Make the button transparent
+        setContentAreaFilled(false); // Don't paint the content area
+        setBorderPainted(false); // Remove the border
+
+        try {
+            spriteImage = ImageIO.read(new File(imagePath)); // Load sprite image from file
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (spriteImage != null) {
+            int x = (getWidth() - spriteImage.getWidth()) / 2;
+            int y = (getHeight() - spriteImage.getHeight()) / 2;
+            g.drawImage(spriteImage, x, y, this);
+        }
+    }
+}
