bc5359c214e3d1cb7c9fe0906d984d3fd1677e70
EliasNe <89510013+EliasNe@users.noreply.github.com>
2023-11-29 13:46:30 +0100

    commita

--- src/test/Java/Model/EnemyOneTest.java
+++ src/test/Java/Model/EnemyOneTest.java
@@ -1,4 +1,5 @@
 package Model;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,7 +15,7 @@ public class EnemyOneTest {
     @Test
     public void next_tile_is_left() {
         ArrayList<Direction> directions = new ArrayList<Direction>();
-        directions.add(Direction.RIGHT); //(3.5, 3.5)
+        directions.add(Direction.RIGHT); // (3.5, 3.5)
         directions.add(Direction.UP);
         directions.add(Direction.RIGHT);
         directions.add(Direction.DOWN);
@@ -23,7 +24,7 @@ public class EnemyOneTest {
         directions.add(Direction.DOWN);
         directions.add(Direction.DOWN);
         AEnemy enemy = new EnemyOne(1, 0.3, directions);
-        //1.5,1.5      2.5,1.5     -----> move(1.5,1)   move()
+        // 1.5,1.5 2.5,1.5 -----> move(1.5,1) move()
 
         enemy.move();
         enemy.move();
@@ -64,8 +65,6 @@ public class EnemyOneTest {
         enemy.move();
         enemy.move();
         enemy.move();
-
-
 
         assertEquals(3.5, enemy.getX());
         assertEquals(3.5, enemy.getY());
