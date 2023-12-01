cb7b3f67d72d280b983e7a4b0058ac97d3ecb362
TobiasOPettersson <tobias.pettersson99@hotmail.com>
2023-12-02 00:24:40 +0100

    comment on subclass

--- src/Main/Java/Model/EnemyOne.java
+++ src/Main/Java/Model/EnemyOne.java
@@ -2,6 +2,10 @@ package Model;
 
 import java.util.List;
 
+/**
+ * A sub-class representing EnemyOne in the game.
+ * This class extends the AEnemy abstract class.
+ */
 public class EnemyOne extends AEnemy{
     
     public EnemyOne(double y, double speed, List<Direction> directions) {
