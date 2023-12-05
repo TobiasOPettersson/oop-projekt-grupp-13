3634be4a74543532e2ce3b1c73927a9164ad4847
EliasNe <89510013+EliasNe@users.noreply.github.com>
2023-12-05 13:38:50 +0100

    some updates om dimension of playbutton

--- src/Main/Java/Controller/PlayButtonController.java
+++ src/Main/Java/Controller/PlayButtonController.java
@@ -12,29 +12,30 @@ import Model.MainModel;
 
 // todo: implement and add an image, maybe an arrow representing "play"
 
-public class PlayButtonController extends JPanel{
+public class PlayButtonController extends JPanel {
     String playImagePath;
     MainModel model;
 
     /**
      * The button that starts a new wave, i.e. starts the game
+     * 
      * @param model is the main model where the method play() is called on
      */
-    public PlayButtonController(MainModel model){
-        this.model = model;   
+    public PlayButtonController(MainModel model) {
+        this.model = model;
         setBackground(Color.red);
-        setSize(150, 75);
+        setSize(120, 75);
         setBorder(BorderFactory.createEmptyBorder(200, 20, 20, 20));
         initButton();
-        //initPlayImage();
+        // initPlayImage();
     }
 
     /**
      * Initializes the button
      */
-    private void initButton(){
+    private void initButton() {
         JButton button = new JButton();
-        button.setSize(125,50);
+        button.setSize(20, 20);
         button.setText("PLAY");
         button.addActionListener(e -> model.play());
         add(button);
@@ -43,7 +44,7 @@ public class PlayButtonController extends JPanel{
     /**
      * Initializes the image of the play button
      */
-    private void initPlayImage(){
+    private void initPlayImage() {
         JLabel playImage = new JLabel(new ImageIcon(playImagePath));
         add(playImage);
     }
